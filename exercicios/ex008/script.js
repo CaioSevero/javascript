40932b85a6982402e55b497929e60a60f070bb22
Caio Severo <caiojapasevero@gmail.com>
2023-05-19 16:20:56 -0300

    ex 08 09 010 011 012 013

---
+++ exercicios/ex008/script.js
@@ -0,0 +1,6 @@
+var vel = 72
+console.log(`A velocidade do seu carro é ${vel}Km/hr` )
+if(vel >60) { //condição simple
+    console.log(`você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`dirija sempre usando cinto de segurança`)
